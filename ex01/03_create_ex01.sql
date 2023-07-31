1f235bd91b89d0e107c491ca1a9cf837d92eb054
GitJanPlata <96839905+GitJanPlata@users.noreply.github.com>
2023-08-01 01:20:36 +0200

    Create/insert_SQL

---
+++ ex01/03_create_ex01.sql
@@ -0,0 +1,10 @@
+-- Tabla atleta
+INSERT INTO atleta VALUES (1, 'Jan Plata');
+INSERT INTO atleta VALUES (2, 'Mike Smith');
+INSERT INTO atleta VALUES (3, 'Richard Roen');
+INSERT INTO atleta VALUES (4, 'Emily Whiskers');
+
+-- Tabla atleta_relevo
+INSERT INTO atleta_relevo VALUES (1, 2);
+INSERT INTO atleta_relevo VALUES (2, 3);
+INSERT INTO atleta_relevo VALUES (3, 4);
